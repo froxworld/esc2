5677830875bf8380e8f922891e83d8636fcbe239
krakenfrancois <froxworld@gmail.com>
2019-09-17 15:51:54 +0200

    add first class and log and database

---
+++ src/main/java/JpaTest.java
@@ -0,0 +1,39 @@
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+
+    public class JpaTest {
+
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+
+            Logger logger = LoggerFactory.getLogger(Main.class);
+            //ATTENTION METTRE LE FICHIER LOG4J.properties à la racine de resources
+            BasicConfigurator.configure();
+
+            EntityManager entityManager = EntityManagerHelper.getEntityManager();
+            EntityTransaction entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+
+
+            try {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            entityTransaction.commit();
+
+
+            entityManager.close();
+            EntityManagerHelper.closeEntityManagerFactory();
+            //factory.close();
+        }
+
+
+    }
